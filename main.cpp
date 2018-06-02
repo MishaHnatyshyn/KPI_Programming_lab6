d32f44789d183aecc83caf31038a0b75c14a1e13
Misha Hnatyshyn <31630903+MishaHnatyshyn@users.noreply.github.com>
2018-06-02 16:20:26 +0300

    simple ray tracing + light

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-
-#include <cmath>
 #include "bmp.h"
+#include <vector>
+#include <omp.h>
+
 using namespace std;
 
-
 struct point{
-    double x;
-    double y;
-    double z;
-    point(double a, double b, double c){
+    float x;
+    float y;
+    float z;
+    point(float a, float b, float c){
         x = a;
         y = b;
         z = c;
@@ -21,94 +21,102 @@ struct point{
     }
 };
 
-struct light{
-    string type;
-    double intensity = 0.6;
-    point position;
-    point direction;
-};
-
-
 struct sphere{
     point centre;
     double radius;
-    sphere(double x, double y, double z, double r){
+    PIXELDATA color;
+    sphere(float x, float y, float z, double R, int r, int g, int b){
         centre.x = x;
         centre.y = y;
         centre.z = z;
-        radius = r;
+        radius = R;
+        color.r = r;
+        color.g = g;
+        color.b = b;
     }
 };
 
-point CanvasToViewport(int x, int y, int Vw,int Cw, int Vh, int Ch, int d){
-    return point(x*Vw/Cw, y*Vh/Ch, d);
-}
+struct canvas{
+    int width;
+    int height;
+};
 
-/*
-    ComputeLighting(P, N) {
-    i = 0.0
-    for light in scene.Lights {
-        if light.type == ambient {
-            i += light.intensity
-        } else {
-            if light.type == point
-                L = light.position - P
-            else
-                L = light.direction
-
-            n_dot_l = dot(N, L)
-            if n_dot_l > 0
-                i += light.intensity*n_dot_l/(length(N)*length(L))
-        }
+vector<vector<PIXELDATA>> putPixel(int x_prev, int y_prev, PIXELDATA color, canvas viewPort, vector<vector<PIXELDATA>> map){
+    int x = viewPort.width/2 + x_prev;
+    int y = viewPort.height/2 - y_prev - 1;
+    if (x < 0 || x >= viewPort.width || y < 0 || y >= viewPort.height) {
+        return map;
     }
-    return i
+    map[y][x] = color;
+    return map;
 }
- */
 
-double ComputeLighting(point P, point N, vector<light> lightSources){
-    double i = 0;
-    for (int j = 0; j < lightSources.size(); ++j) {
-        if (lightSources[j].type == "ambient"){
-            i+=lightSources[j].intensity;
-        } else{
-            point L;
-            if(lightSources[j].type == "point"){
-                L.x = lightSources[j].position.x - P.x;
-                L.y = lightSources[j].position.y - P.y;
-                L.x = lightSources[j].position.z - P.z;
-            }else{
-                L.x = lightSources[j].position.x;
-                L.y = lightSources[j].position.y;
-                L.x = lightSources[j].position.z;
-            }
-            double lengthN = sqrt(N.x * N.x + N.y * N.y + N.z * N.z);
-            double lengthL = sqrt(L.x * L.x + L.y * L.y + L.z * L.z);
-            double n_dot_l = N.x * L.x + N.y * L.y + N.z * L.z;
-            if (n_dot_l > 0) {
-                i += lightSources[j].intensity * n_dot_l / (lengthL * lengthN);
-            }
-        }
-
+struct light{
+    string type;
+    double intensity;
+    point pos;
+    light(string t, double i){
+        type = t;
+        intensity = i;
     }
-    return i;
+    light(string t, double i, double x, double y, double z){
+        type = t;
+        intensity = i;
+        pos.x = x;
+        pos.y = y;
+        pos.z = z;
+    }
+};
+
+double dot(point v1, point v2){
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
+point subtract(point v1, point v2){
+    return {v1.x-v2.x, v1.y - v2.y, v1.z - v2.z};
+}
+point add(point v1, point v2){
+    return {v1.x+v2.x, v1.y + v2.y, v1.z + v2.z};
+}
+point multiply(double k, point v){
+    return {v.x*k, v.y*k, v.z*k};
+}
+
+double max(double a, double b){
+    if (a>b) return a;
+    else return b;
+}
+
+double min(double a, double b){
+    if (a<b) return a;
+    else return b;
+}
+
+PIXELDATA clamp(point v){
+    return {min(255.0, max(0.0,v.x)), min(255, max(0.0,v.y)), min(255, max(0.0,v.z))};
+}
+
+double length(point v1){
+    return sqrt(dot(v1,v1));
+}
+
+point CanvasToViewport(int x, int y, int d,canvas canv, int viewport_size){
+    point res(((double)x*viewport_size)/(double)canv.width, ((double)y*viewport_size)/(double)canv.height, d);
+    return res;
+}
 
 vector<double> IntersectRaySphere(point O, point D, sphere obj){
     vector<double> res(2);
     point C = obj.centre;
     double r = obj.radius;
-    point oc;
-    oc.x = O.x - C.x;
-    oc.y = O.y - C.y;
-    oc.z = O.z - C.z;
-    double k1 = D.x*D.x + D.y*D.y + D.z*D.z;
-    double k2 = 2 * (oc.x*D.x + oc.y*D.y + oc.z*D.z);
-    double k3 = (oc.x*oc.x + oc.y*oc.y + oc.z*oc.z) - r*r;
+    point oc = subtract(O,obj.centre);
+    double k1 = dot(D,D);
+    double k2 = 2 * dot(oc,D);
+    double k3 = dot(oc,oc) - r*r;
     double discriminant = k2*k2 - 4*k1*k3;
     if (discriminant < 0) {
-        res[0] = 100000001;
-        res[1] = 100000001;
+        res[0] = 100000001000;
+        res[1] = 100000001000;
         return res;
     }
 
@@ -119,91 +127,103 @@ vector<double> IntersectRaySphere(point O, point D, sphere obj){
     return res;
 }
 
-PIXELDATA TraceRay(point O, point D, int t_min, int t_max, sphere obj){
+double ComputeLighting(point O, point N, vector<light> lights){
+    double intensity = 0;
+    double length_n = length(N);
+    for (int i = 0; i < lights.size(); ++i) {
+        light curr_light = lights[i];
+        if(curr_light.type == "ambient"){
+            intensity += curr_light.intensity;
+        } else{
+            point vec_l;
+            if(curr_light.type == "point"){
+                vec_l = subtract(curr_light.pos, O);
+            } else{
+                vec_l = curr_light.pos;
+            }
+            double n_dot_l = dot(N, vec_l);
+            if (n_dot_l > 0){
+                intensity += curr_light.intensity*n_dot_l/(length_n * length(vec_l));
+            }
+        }
+    }
+    return intensity;
+}
+
+
+PIXELDATA TraceRay(point O, point D, int t_min, int t_max, vector<sphere> obj, vector<light> lights){
     PIXELDATA res;
     res.r = 255;
     res.g = 255;
     res.b = 255;
     double closest_t = 100000001;
     sphere* closest_sphere = nullptr;
-    vector<double> t = IntersectRaySphere(O, D, obj);
-    double t1 = t[0];
-    double t2 = t[1];
-    if (t1 > t_min && t1 < t_max && t1 < closest_t){
-        closest_t = t1;
-        res.r = 0;
-        res.g = 0;
-        res.b = 0;
+    for (int i = 0; i < obj.size(); ++i) {
+        vector<double> t = IntersectRaySphere(O, D, obj[i]);
+        if (t[0] > t_min && t[0] < t_max && t[0] < closest_t){
+            closest_t = t[0];
+            closest_sphere = &obj[i];
+        }
+        if (t[1] > t_min && t[1] < t_max && t[1] < closest_t){
+            closest_t = t[1];
+            closest_sphere = &obj[i];
+        }
     }
-    if (t2 > t_min && t2 < t_max && t2 < closest_t){
-        closest_t = t2;
-        res.r = 0;
-        res.g = 0;
-        res.b = 0;
+    if (closest_sphere == nullptr){
+        return res;
     }
-    point P(O.x+D.x*closest_t, O.y+D.y*closest_t, O.z+D.z*closest_t);
-    point N(P.x - obj.centre.x, P.y - obj.centre.y, P.z - obj.centre.z);
-    double lengthN = sqrt(N.x*N.x + N.y*N.y + N.z*N.z);
-    point N1(N.x/lengthN, N.y/lengthN, N.z/lengthN);
-    light lightSourcePoint;
-    light lightSourceAmbient;
-    light lightSourceDirectional;
-    lightSourceAmbient.type = "ambient";
-    lightSourceDirectional.type = "directional";
-    lightSourceDirectional.intensity = 0.2;
-    lightSourceDirectional.direction = point(3,5,4);
-    lightSourceAmbient.intensity = 0.2;
-    lightSourcePoint.type = "point";
-    lightSourcePoint.intensity = 0.6;
-    lightSourcePoint.position.x = 200;
-    lightSourcePoint.position.y = 200;
-    lightSourcePoint.position.z = 200;
-    vector<light> lightSources;
-    lightSources.push_back(lightSourcePoint);
-    lightSources.push_back(lightSourceDirectional);
-    lightSources.push_back(lightSourceAmbient);
 
-    double i = ComputeLighting(P, N1, lightSources);
-    PIXELDATA res1(res.r*i, res.g*i, res.b*i);
-    return res1;
+    point point1 = add(O, multiply(closest_t, D));
+    point normal = subtract(point1, closest_sphere->centre);
+    normal = multiply(1.0/length(normal), normal);
+    double i = ComputeLighting(point1,normal,lights);
+    res.r = closest_sphere->color.r * i;
+    res.g = closest_sphere->color.g * i;
+    res.b = closest_sphere->color.b * i;
+
+    return res;
 }
 
 
-int main() {
-    point O(0,0,0);
-    int d = 400;
-    int Cw = 1000;
-    int Ch = 1000;
-    int Vh = 400;
-    int Vw = 400;
-    vector<PIXELDATA> row(Vw);
-    sphere mySphere(50,50,600,37);
-    vector<vector<PIXELDATA>> viewPort;
-    for (int i = 0; i < Vh; ++i) {
-        viewPort.push_back(row);
+int main(){
+    canvas canv;
+    canv.width = 200;
+    canv.height = 200;
+    int viewport_size = 1;
+    int projection_plane_z = 1;
+    point camera_position(0,0,0);
+    vector<sphere> spheres;
+    vector<light> lights;
+    vector<PIXELDATA> row(canv.width);
+    vector<vector<PIXELDATA>> map;
+    for (int i = 0; i < canv.height; ++i) {
+        map.push_back(row);
     }
-    for (int x = (-1)*Cw/2; x < Cw/2; x++) {
-        for (int y = (-1)*Ch/2; y < Ch/2; y++) {
-            point D = CanvasToViewport(x,y,Vw,Cw,Vh,Ch,d);
-            PIXELDATA color = TraceRay(O,D,0,300,mySphere);
-            int matrixXPos = Vw/2 + x;
-            int matrixYPos = Vh/2 + y;
-            if (matrixXPos >= Vw ||
-                matrixYPos >= Vh ||
-                matrixXPos < 0 ||
-                matrixYPos < 0) continue;
-            viewPort[matrixXPos][matrixYPos] = color;
+
+    //omp_set_num_threads(2);
+    spheres.push_back(sphere(0, -1, 3 , 1, 255, 0, 0));
+    spheres.push_back(sphere(0, 0, 3 , 1, 0, 0, 255));
+    spheres.push_back(sphere(-2, 0, 4 , 1, 0, 255, 0));
+    lights.push_back(light("ambient", 0.2));
+    lights.push_back(light("point", 0.6, 2, 1, 0));
+    lights.push_back(light("directional", 0.2, 1, 4, 4));
+
+
+    #pragma omp parallel private(x, y)
+    #pragma omp for collapse(2) schedule(dynamic, CHUNK)
+
+    for (int x = -canv.width/2; x < canv.width/2; x++) {
+        for (int y = -canv.height/2; y < canv.height/2; y++) {
+            point direction = CanvasToViewport(x,y,projection_plane_z,canv,viewport_size);
+            PIXELDATA color = TraceRay(camera_position, direction, 1, 100000001, spheres, lights);
+            map = putPixel(x,y,color,canv,map);
+
         }
+    cout << x << endl;
     }
-/*
-    for (int j = 0; j < Vh; ++j) {
-        for (int i = 0; i < Vw; ++i) {
-            cout<<viewPort[j][i]<<"  ";
-        }
-        cout << endl;
-    }*/
 
-    create("test.bmp",Vw,Vh,viewPort);
 
+
+    create("res.bmp", canv.width, canv.height, map);
     return 0;
 }
