8985ca29429adc2172519a5423efcf97b9ab8c62
VladaZhmurkevych <31669221+VladaZhmurkevych@users.noreply.github.com>
2018-06-04 00:07:17 +0300

    Update Octree.cpp

--- Octree.cpp
+++ Octree.cpp
@@ -29,12 +29,12 @@ void add_node(point minPoint, point maxPoint, Octree *&MyTree ) {
             MyTree->children[k] = new Octree;
             for (int i = 0; i < 8; i++) {
                 MyTree->children[k]->children[i] = NULL;
-                MyTree->children[k]->minPoint.x = (k - 1) * (MyTree->minPoint.x + maxPoint.x) / 8;;
-                MyTree->children[k]->minPoint.y = (k - 1) * (MyTree->minPoint.y + maxPoint.y) / 8;;
-                MyTree->children[k]->minPoint.z = (k - 1) * (MyTree->minPoint.z + maxPoint.z) / 8;;
-                MyTree->children[k]->maxPoint.x = k * (MyTree->minPoint.x + maxPoint.x) / 8;
-                MyTree->children[k]->maxPoint.y = k * (MyTree->minPoint.y + maxPoint.y) / 8;;
-                MyTree->children[k]->maxPoint.z = k * (MyTree->minPoint.z + maxPoint.z) / 8;;
+                MyTree->children[k]->minPoint.x = k * (MyTree->minPoint.x + maxPoint.x) / 8;;
+                MyTree->children[k]->minPoint.y = k * (MyTree->minPoint.y + maxPoint.y) / 8;;
+                MyTree->children[k]->minPoint.z = k * (MyTree->minPoint.z + maxPoint.z) / 8;;
+                MyTree->children[k]->maxPoint.x = (k+1) * (MyTree->minPoint.x + maxPoint.x) / 8;
+                MyTree->children[k]->maxPoint.y = (k+1) * (MyTree->minPoint.y + maxPoint.y) / 8;;
+                MyTree->children[k]->maxPoint.z = (k+1) * (MyTree->minPoint.z + maxPoint.z) / 8;;
             }
         }
     }
